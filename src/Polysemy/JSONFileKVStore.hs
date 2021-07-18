5df04184e3634ffaef9a85e32257ec2e228ead43
Daniel Firth <dan.firth@homotopic.tech>
2021-07-18 12:09:23 +0200

    Inline and update stack to 2021-07-17

--- src/Polysemy/JSONFileKVStore.hs
+++ src/Polysemy/JSONFileKVStore.hs
@@ -1,25 +1,31 @@
-{-# LANGUAGE BlockArguments      #-}
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE DeriveGeneric       #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications    #-}
-{-# LANGUAGE TypeOperators       #-}
-module Polysemy.JSONFileKVStore where
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Polysemy.JSONFileKVStore
+  ( JSONParseException (..),
+    runKVStoreAsJSONFileStore,
+    eitherDecodeOrCreate,
+  )
+where
 
 import Control.Monad.Catch
 import Control.Monad.Extra
 import Control.Monad.IO.Class
 import Data.Aeson
-import qualified Data.Map as Map
 import Data.Map (Map)
+import qualified Data.Map as Map
 import GHC.Generics
+import Path
 import Polysemy
 import Polysemy.Error
 import Polysemy.KVStore
-import Path
 import qualified UnliftIO.Path.Directory as U
 
 newtype JSONParseException = JSONParseException String
@@ -33,18 +39,29 @@ eitherDecodeOrCreate f x = do
   whenM (fmap not . U.doesFileExist $ f) $ liftIO $ encodeFile (toFilePath f) x
   liftIO $ eitherDecodeFileStrict' (toFilePath f)
 
-runKVStoreAsJSONFileStore :: (Members '[Embed IO, Error JSONParseException] r,
-                              FromJSONKey k, ToJSONKey k, FromJSON v, ToJSON v, Ord k)
-                          => Path b File
-                          -> Sem (KVStore k v ': r) a -> Sem r a
+-- | Run a `KVStore` as a json file stored as a `Map`.
+--
+-- @since 0.1.0.0
+runKVStoreAsJSONFileStore ::
+  ( Members '[Embed IO, Error JSONParseException] r,
+    FromJSONKey k,
+    ToJSONKey k,
+    FromJSON v,
+    ToJSON v,
+    Ord k
+  ) =>
+  Path b File ->
+  Sem (KVStore k v ': r) a ->
+  Sem r a
 runKVStoreAsJSONFileStore d = interpret \case
   LookupKV k -> do
     z <- eitherDecodeOrCreate d mempty
     case z of
-      Left x  -> throw @JSONParseException $ JSONParseException x
+      Left x -> throw @JSONParseException $ JSONParseException x
       Right x -> return $ Map.lookup k x
   UpdateKV k v -> do
     z <- eitherDecodeOrCreate d mempty
     case z of
       Left x -> throw $ JSONParseException x
       Right (x :: Map k v) -> embed $ encodeFile (toFilePath d) (Map.alter (const v) k x)
+{-# INLINE runKVStoreAsJSONFileStore #-}
